bbb163432d7acc0b2f969403ed90ac42800f2a6c
arun <asvprogrammer2015@gmail.com>
2023-01-22 23:35:37 +0530

    Initial Commit

--- web.py
+++ web.py
@@ -14,8 +14,12 @@ st.title("To-Do Web App")
 st.subheader("This is my todo web app")
 st.write("This app increases productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label="", placeholder="Add new todo...", on_change=add_todo, key="new_todo")
-
